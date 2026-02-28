a8fff8c9e596abcb1585250db7259fb360ff72bc
kesfet <kesfet@zohomail.in>
2026-02-28 12:57:42 +0000

    Update pktLearn.cpp

--- pktop/pktLearn.cpp
+++ pktop/pktLearn.cpp
@@ -1,7 +1,7 @@
 #include "/include/PktR.hpp"
 #include "/include/utils.hpp"
 
-void PacketReader(int fd){
+void PacketReader(int fd,TCP_CON_TABLE* tcp_table){
 
     uint8_t buffer[4096]={0};
 
@@ -13,9 +13,9 @@ void PacketReader(int fd){
 
     struct iphdr ip = {0};
     
-    if (ip.version != 4) return;
     if (nread < sizeof(iphdr))    return;
     memcpy(&ip,buffer,sizeof(iphdr));
+    if (ip.version != 4) return;
 
     uint32_t ip_header_len = {0};
     ip_header_len = ip.ihl * 4;
@@ -44,9 +44,33 @@ void PacketReader(int fd){
             memcpy (&tcp,    buffer+ip_header_len,   sizeof(tcphdr));
 
             uint32_t tcp_header_len = tcp.doff * 4;
-            if (tcp_header_len < 20) return;
+            if (tcp_header_len < TCP_HEADER_LEN) return;
             if (nread < ip_header_len + tcp_header_len) return;
-        
+
+                struct TcpPacketContext pkt_data;
+                pkt_data.key.SourceIP           = ip.saddr;
+                pkt_data.key.DestinationIP      = ip.daddr;
+                pkt_data.key.SourcePort         = ntohs(tcp.source);;
+                pkt_data.key.DestinationPort    = ntohs(tcp.dest);
+
+                if (tcp.syn && !tcp.ack){
+                    pkt_data.meta.State = TcpState::RECEIVED;
+                    pkt_data.meta.SeqRecv = ntohl(tcp.seq);
+                }else if (tcp.syn && tcp.ack){
+                    pkt_data.meta.State = TcpState::SYN_ACKED;
+                    pkt_data.meta.SeqRecv = ntohl(tcp.seq);
+                    pkt_data.meta.SeqSent = ntohl(tcp.ack_seq);
+                }else if (tcp.ack && !tcp.syn && !tcp.fin) {
+                    pkt_data.meta.State   = TcpState::ESTABLISHED;
+                    pkt_data.meta.SeqRecv = ntohl(tcp.seq);
+                    pkt_data.meta.SeqSent = ntohl(tcp.ack_seq);
+                }else if (tcp.fin){
+                    pkt_data.meta.State = TcpState::CLOSED;
+                }else{
+                    //
+                }
+
+                PacketHandler(pkt_data , tcp_table);
             break;
         default:
             return;
